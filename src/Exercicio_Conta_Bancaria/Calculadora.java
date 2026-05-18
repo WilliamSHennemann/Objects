6ad6bcbbfab8afab45fad67bee8c37f8a51c6496
Arthur kehl <williamdossantoshennemann@gmail.com>
2026-05-18 09:35:19 -0300

    ok

--- src/Exercicio_Conta_Bancaria/Calculadora.java
+++ src/Exercicio_Conta_Bancaria/Calculadora.java
@@ -3,5 +3,28 @@ package Exercicio_Conta_Bancaria;
 public class Calculadora {
     static void main() {
         Operacoes operacoes = new Operacoes();
+
+        float n1, n2;
+
+
+        n1 = Float.parseFloat(IO.readln("Digite o primeiro número da soma: "));
+        n2 = Float.parseFloat(IO.readln("Digite o segundo número da soma: "));
+        System.out.println("Resultado da soma: " + operacoes.soma(n1, n2));
+
+
+        n1 = Float.parseFloat(IO.readln("Digite o primeiro número da subtração: "));
+        n2 = Float.parseFloat(IO.readln("Digite o segundo número da subtração: "));
+        System.out.println("Resultado da subtração: " + operacoes.subtracao(n1, n2));
+
+        n1 = Float.parseFloat(IO.readln("Digite o primeiro número da multiplicação: "));
+        n2 = Float.parseFloat(IO.readln("Digite o segundo número da multiplicação: "));
+        System.out.println("Resultado da multiplicação: " + operacoes.multiplicacao(n1, n2));
+
+        n1 = Float.parseFloat(IO.readln("Digite o primeiro número da divisão: "));
+        n2 = Float.parseFloat(IO.readln("Digite o segundo número da divisão: "));
+        System.out.println("Resultado da divisão: " + operacoes.divisao(n1, n2));
+
+        n1 = Float.parseFloat(IO.readln("Digite o número para elevar ao quadrado: "));
+        System.out.println("Resultado do quadrado: " + operacoes.exponenciacao(n1));
     }
 }
--- src/Exercicio_Conta_Bancaria/Operacoes.java
+++ src/Exercicio_Conta_Bancaria/Operacoes.java
@@ -1,12 +1,25 @@
 package Exercicio_Conta_Bancaria;
 public class Operacoes {
 
+    float soma;
+    float subtracao;
+    float multiplicacao;
+    float exponenciacao;
+
     float soma (float n1, float n2){
-        return n1 + n2;  }
+        return n1 + n2;
+    }
     float subtracao (float n1, float n2){
         return n1 - n2;  }
     float multiplicacao (float n1, float n2){
         return n1 * n2;  }
+    float divisao (float n1, float n2){
+        if (n2 != 0) {
+            return n1 / n2;
+        } else {
+            System.out.println("Erro: divisão por zero!");
+            return 0;
+        }  }
     float exponenciacao (float n1){
         return n1 * n1;  }
 }
